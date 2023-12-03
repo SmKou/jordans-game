cba169cd6875940ace836f5e926f1f8e6bbaaec4
Sm Kou <66178856+SmKou@users.noreply.github.com>
2023-12-03 00:16:33 -0800

    WIP Add 16x16 lattice grids to canvas (testing re-render with minute details)

--- vite-app/app/render.js
+++ vite-app/app/render.js
@@ -3,10 +3,35 @@ const drawBackground = ({ ctx, width, height }) => {
     ctx.fillRect(0, 0, width, height)
 }
 
+const complexLattice = ({ ctx, tile, x, y }) => {
+    const quart = Math.floor(tile / 4)
+
+    ctx.strokeStyle = '#000'
+
+    for (let i = 0; i < 16; ++i) {
+        const x_i = i % 4 * 4 + x
+        const y_i = Math.floor(i / 4) * 4 + y
+        ctx.beginPath()
+        ctx.moveTo(x_i, y_i)
+        ctx.lineTo(x_i + quart, y_i + quart)
+        ctx.moveTo(x_i + quart, y_i)
+        ctx.lineTo(x_i, y_i + quart)
+        ctx.moveTo(x_i, y_i)
+        ctx.closePath()
+        ctx.stroke()
+    }
+}
+
 const gesso = view => {
 
     const draw = () => {
         drawBackground(view)
+
+        const { ctx, tile } = view
+
+        for (let i = 1; i < view.width; i += 16)
+            for (let j = 1; j < view.height; j += 16)
+                complexLattice({ ctx, tile, x: i, y: j })
     }
 
     const redraw = () => {
