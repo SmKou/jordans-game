9c5e797730e8ccf2c6ee04c69685340643bf9a74
Sm Kou <66178856+SmKou@users.noreply.github.com>
2024-02-01 14:58:20 -0800

    WIP Add geo scripting

---
+++ geo.js
@@ -0,0 +1,100 @@
+class geo {
+    constructor(geo) {
+        this.peum = {}
+        this.geo = {}
+        this.fig = {}
+        this.origin = geo
+    }
+
+    do() {}
+
+    attack(target) {}
+    defend(attack) {}
+
+}
+
+const moves = { }
+
+const geos = {
+    'a.f.r': { name: "Afarag", type: 'g', base: 'western dragon',
+        moves: {
+            dark: [],
+            light: []
+        }
+    },
+    'm.o': { name: "Me'opt", type: 'p.t', base: 'cat',
+        moves: {
+            dark: [],
+            light: []
+        }
+    },
+    'm.m.r': { name: "Mamarok", type: 'k', base: 'sheep',
+        moves: {
+            dark: [],
+            light: []
+        }
+    },
+    'p.t': [
+        { name: "Pitak", type: 'k', base: 'mouse',
+            moves: {
+                dark: [],
+            light: []
+            }
+        },
+        { name: "Pito", type: 'p', base: ['fighter', 'wrestler'],
+            moves: {
+                dark: [],
+                light: []
+            },
+            variants: {
+                'l': { name: "Pitoli", base: ['kickboxer'],
+                    moves: {
+                        dark: [],
+                        light: []
+                    }
+                },
+                'n': { name: "Pitone", base: ['boxer'],
+                    moves: {
+                        dark: [],
+                        light: []
+                    }
+                },
+                't': { name: "Pitota", base: ['dancer'],
+                    moves: {
+                        dark: [],
+                        light: []
+                    }
+                }
+            }
+        }
+    ],
+    'd': { type: 's.n', base: 'rabbit',
+        moves: {
+            dark: [],
+            light: []
+        },
+        variants: {
+            'r': { name: "Dorsn", base: 'feminine',
+                moves: {
+                    dark: [],
+                    light: []
+                }
+            },
+            '': { name: "Dosnr", base: 'masculine',
+                moves: {
+                    dark: [],
+                    light: []
+                }
+            }
+        }
+    }
+}
+
+const lexi = {
+    'g': 'dragon',
+    'r': 'unknown',
+    'a': 'target',
+    'e': 'attack',
+    'e.e': 'move',
+    'u': 'type'
+}
