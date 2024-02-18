729fc9c3a1120021feb0e0a71feff74de56bed2b
Sm Kou <sm2smkou.d@gmail.com>
2024-02-17 19:30:55 -0800

    WIP Add creature class

--- ui/game.js
+++ ui/game.js
@@ -34,6 +34,18 @@
     Sell (item)
 */
 
+class Creature {
+    constructor() {
+        
+    }
+
+
+}
+
+class Geo extends Creature {}
+class Fig extends Creature {}
+class Peum extends Creature {}
+
 const eat = (food = '') => {
     if (!food || food.type !== 'food') return;
 }
