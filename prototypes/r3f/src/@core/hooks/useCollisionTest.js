3206e1d9807b83e1c190e9b5e7c0033d13cac388
Sm Kou <66178856+SmKou@users.noreply.github.com>
2024-03-07 14:03:49 -0800

    Recovery point on Small

---
+++ prototypes/r3f/src/@core/hooks/useCollisionTest.js
@@ -0,0 +1,33 @@
+import { useCallback } from "react"
+
+
+export default function useCollisionTest({ sight = false, hit = false }) {
+    const { findGameObjectsByXY } = useGame()
+    const { id } = useGameObject() || {}
+    return useCallback(
+        (position) => {
+            const { x, y } = position
+            const gameObjectsAtXY = findGameObjectsByXY(x, y)
+            if (!gameObjectsAtXY.length)
+                return false
+            return gameObjectsAtXY.every(gameObj => {
+                if (gameObj.id === id)
+                    return true
+                const collider = gameObj.getComponent('Collider')
+                if (sight) 
+                    return (gameObj.layer == null
+                        || (gameObj.layer !== 'wall' && gameObj.layer !== 'obstacle')
+                        || (collider && collider.walkable))
+                if (hit)
+                    return (gameObj.layer == null
+                        || (gameObj.layer !== 'wall' 
+                            && gameObj.layer !== 'visible-wall' 
+                            && gameObj.layer !== 'obstacle')
+                        || (collider && collider.walkable)
+                    )
+                return !collider || collider.walkable
+            })
+        },
+        [id, findGameObjectsByXY, sight, hit]
+    )
+}
